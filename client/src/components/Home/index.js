3e5d273a94f76bc90f78e63673f68695d7160266
Maksim <sokolovskiy-11111@mail.ru>
2020-10-12 16:16:35 +0300

    feat: fixed bugs from commit and create List Show

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -2,96 +2,74 @@ import React, { useState } from 'react';
 import styles from './styles.module.css';
 import Input from 'components/common/Input';
 import Button from 'components/common/Button';
+import Table from 'components/common/Table';
+import List from 'components/common/List';
 import empty from 'assets/img/emptyImg.png';
 import point from 'assets/img/vertical_point.png';
 import searchicon from 'assets/img/searchicon.png';
 import filter from 'assets/img/filter.png';
 import grid from 'assets/img/grid.png';
 import row from 'assets/img/row.png';
+import { ReactComponent as OkIcon } from 'assets/img/ok.svg';
 
 
 
 
-function Home() {
+
+function Home({ contacts }) {
     const [modalState, setModalState] = useState(false);
-    const [contacts, setContacts] = useState([
-        { name: 'Paul', job: 'CEO', decision: 'Deside', promoter: 'Promoter', level: "High", relatOwner: 'Laura' },
-        { name: 'Maksim', job: 'CTO', decision: 'Deside', promoter: 'Strong Promoter', level: "Low", relatOwner: 'John' },
-        { name: 'Andrey', job: 'CMO', decision: 'Agree', promoter: 'Detractor', level: "Emerging", relatOwner: 'Laura' },
-        { name: 'Vadim', job: 'Accountant', decision: 'Recommend', promoter: 'Neutral', level: "Medium", relatOwner: 'John' },
-    ]);
     const [isContactsTable, setIsContactsTable] = useState(true);
 
-    if (contacts.length == 0) {
-        return (
-            <div className={styles.home}>
-                <div className={styles.menu}>
-                    <button>LogOut</button>
-                </div>
-                <div className={styles.main}>
-                    <div className={styles.emptyContact}>
-                        <h1>Power Base</h1>
-                        <p>Identify the people in this account that matter</p>
-                        <Button handleClick={() => { setModalState(!modalState); console.log(modalState) }} text="Add New Priority Contact" />
-                    </div>
-                    <img src={empty} alt="" />
-                </div>
+    const showTable = (target) => {
+        setIsContactsTable(true);
+    }
+    const showList = (target) => {
+        setIsContactsTable(false);
+    }
+    let activeTable;
+    let activeList;
 
-            </div>
-        )
-    } else {
-        return (
-            <div className={styles.home}>
-                <div className={styles.menu}>
-                    <button>LogOut</button>
-                </div>
-                <div className={styles.notEmptyContact}>
-                    <h2>Power Base</h2>
-                    <div className={styles.sort}>
-                        <Input type="text" placeholder="Search" />
-                        <img className={styles.searchIcon} src={searchicon} alt="" />
-                        <img className={styles.filter} src={filter} alt="" />
-                        <img src={row} className={styles.active} alt="" />
-                        <img src={grid} alt="" />
+    isContactsTable ? (activeTable = {
+        background: '#efefef'
+    }) :
+        (activeList = {
+            background: '#efefef'
+        });
 
+    let searchInputHandler = () => {
 
-                    </div>
-                    {isContactsTable ? <table>
-                        <thead>
-                            <tr>
-                                <td>Name</td>
-                                <td>Job Title</td>
-                                <td>Priority Level</td>
-                                <td>Promoter/Detractor</td>
-                                <td>Decision Right</td>
-                                <td>Relationship Owner</td>
-                                <td>PriorityTopics</td>
-                                <td>&nbsp;</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {contacts.map((contact, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{contact.name}</td>
-                                        <td>{contact.job}</td>
-                                        <td>{contact.level}</td>
-                                        <td>{contact.promoter}</td>
-                                        <td>{contact.decision}</td>
-                                        <td>{contact.relatOwner}</td>
-                                        <td>None</td>
-                                        <td><img className={styles.editImg} src={point} alt="" /></td>
-                                    </tr>
-                                )
-                            })}
-
-                        </tbody>
-                    </table> : <span>False</span>}
-                </div>
-            </div >
-        )
     }
 
+    return (
+        <div className={styles.home}>
+            <div className={styles.menu}>
+                <button>LogOut</button>
+                {/* <OkIcon /> */}
+            </div>
+            {
+                (contacts.length === 0) ?
+                    <div className={styles.main}>
+                        <div className={styles.emptyContact}>
+                            <h1>Power Base</h1>
+                            <p>Identify the people in this account that matter</p>
+                            <Button handleClick={() => { setModalState(!modalState) }} text="Add New Priority Contact" />
+                        </div>
+                        <img src={empty} alt="" />
+                    </div> :
+                    <div className={styles.notEmptyContact}>
+                        <h2>Power Base</h2>
+                        <div className={styles.sort}>
+                            <Input type="text" placeholder="Search" onChange={(e) => searchInputHandler(e.target.value)} />
+                            <img className={styles.searchIcon} src={searchicon} alt="" />
+                            <img className={styles.filter} src={filter} alt="" />
+                            <img onClick={(e) => showTable(e.target)} className={isContactsTable ? styles.active : styles.notActive} src={row} style={activeTable} alt="" />
+                            <img onClick={(e) => showList(e.target)} src={grid} style={activeList} alt="" />
+                        </div>
+                        {isContactsTable ? <Table contacts={contacts} /> : <List contacts={contacts} />}
+                    </div>
+            }
+        </div>
+    )
 }
 
 export default Home;
